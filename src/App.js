362bde6aebb24abd931fe102b6bc4b9897db8d1a
Rayhaan Ugharadar <rayhaanugharadar@Rayhaans-MacBook-Pro.local>
2022-04-11 15:48:18 +0100

    display flexed both lanuage lists

--- src/App.js
+++ src/App.js
@@ -1,53 +1,17 @@
 import { Firestore, collection, getDocs, QuerySnapshot } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
 import './App.css';
+import { Language } from './components/Language';
 import { db } from './firebase';
 
 
 function App() {
-  let arr = []
-  const insteadOf = []
-  const use = []
-  const[tryList, setTryList] = useState([])
-  const [insteadOfList, setInsteadOfList] = useState([])
-  const [message, setMessage] = useState('')
-  
-  
-  
-  
-
-  useEffect(() => {
-   getDocs(collection(db, "instead-of"))
-  .then((snapshot) => {
-    snapshot.docs.forEach((doc) => {
-      arr.push({...doc.data()})
-    })
-  })
-  .then(() => {
-   arr.forEach((doc) => {
-     insteadOf.push(doc.instead)
-     use.push(doc.try)
-   })
-  })
-  .then(() => {
-    setInsteadOfList(insteadOf)
-    setTryList(use)
-  })
-  }, [])
 
   return (
     <div className="App">
       <header className="App-header">
-        <h3>Instead of</h3>
-       <ul>
-       {insteadOfList.map((doc, index) => <li key={index}>{doc}</li>)}
-       </ul>
 
-    <h3>Try</h3>
-    <ul>
-      {tryList.map((doc, index) => <li key={index}>{doc}</li>)}
-    </ul>
-
+      <Language />
       </header>
     </div>
   );
